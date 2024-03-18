ad6ce403814274c3feacab71ec9a940ee8e6e44c
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-18 21:39:44 +0530

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Manvendu Pathak on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
